e590c93fb6a1594dfded1c0e199bc52f80321059
brellin <cekakid@gmail.com>
2022-09-06 11:49:23 -0500

    change name to app

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const server = require('./api');
+const app = require('./api');
 
 const port = process.env.PORT || 5000;
 
-server.listen(port, _ => {
+app.listen(port, _ => {
     const portString = `Server running on port ${ port }`;
     const blockade = '='.repeat(portString.length + 2);
     console.log(`\n${ blockade }\n ${ portString }\n${ blockade }\n`);
